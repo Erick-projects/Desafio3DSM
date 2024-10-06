2edda25c4a70832f20e7817aed5d9de8dec4439a
Erick_Ramos <ericklm1019@gmail.com>
2024-10-05 20:17:22 -0600

    Desafio completo

---
+++ RecursosDeAprendizaje/app/src/main/java/com/udb/recursosdeaprendizaje/Recurso.kt
@@ -0,0 +1,10 @@
+package com.udb.recursosdeaprendizaje
+
+data class Recurso(
+    val id: Int,
+    val titulo: String,
+    val descripcion: String,
+    val tipo: String,
+    val enlace: String,
+    val imagen: String
+)
